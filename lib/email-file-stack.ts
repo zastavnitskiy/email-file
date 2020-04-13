bc214e38847369c286f8a0490d14f730256444a5
Pavel Zastavnitskiy <zastavnitskiy@Pavels-MacBook-Pro.local>
2020-04-13 11:35:06 +0200

    Setup lamda to work with a custom domain

--- lib/email-file-stack.ts
+++ lib/email-file-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as targets from '@aws-cdk/aws-route53-targets';
+import {Certificate} from '@aws-cdk/aws-certificatemanager';
+
+const domainName = 'email.zastavnitskiy.dev';
 
 export class EmailFileStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,9 +17,34 @@ export class EmailFileStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", function is "handler"
     });
+
+    const certificate = new Certificate(this, 'EndpointCertificate', {
+      domainName
+    })
+
     
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+    const restApi = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+      options: {
+        domainName: {
+          domainName,
+          certificate
+        }
+      }
+    });
+
+    /**
+     * Once deployed, point your subdomain nameservers to 
+     * the AWS ones. You can find them in Route53 console,
+     * in the properties of the created hosted zone.
+     */
+    const zone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: domainName
+    })
+
+    new route53.ARecord(this, 'AliasRecord', {
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.ApiGateway(restApi))
     });
   }
 }
